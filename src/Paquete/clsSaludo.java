33528b7ee4e43c715949695fce1e7e41ca626fbf
Adminfoco <Adminfoco@10.0.0.7>
2020-12-21 22:07:29 -0500

    prueba con github

---
+++ src/Paquete/clsSaludo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete;
+
+/**
+ *
+ * @author Adminfoco
+ */
+public class clsSaludo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo Java"); 
+    }
+}
